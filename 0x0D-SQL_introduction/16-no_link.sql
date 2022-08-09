88d755b5984a7f5b2cb3ec3f18b5999d576dfa40
Abdulahi Ogundare <99307799+Obapythonaire@users.noreply.github.com>
2022-08-09 16:33:19 +0100

    Updated task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,7 +1,5 @@
 -- Lists all records of the table "second_table"
 	-- without listing rows without a name value
 		--in descending order
-SELECT `score`, `name`
-FROM `second_table`
-WHERE `name` != ""
-ORDER BY `score` DESC
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+
