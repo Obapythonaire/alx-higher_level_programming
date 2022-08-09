e82c739ebeb63a4384b9370ca818441f44b16c73
obapythonaire <abdulahogundare@gmail.com>
2022-08-09 11:34:49 +0100

    Introduction to SQL

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,7 @@
+-- Lists all records of the table "second_table"
+	-- without listing rows without a name value
+		--in descending order
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
